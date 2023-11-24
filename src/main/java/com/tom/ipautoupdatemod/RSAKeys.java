b7fa3c4a1e090b98749f49c45b51be70a4369bf5
Tom Wu <tomwu370@gmail.com>
2023-11-24 01:08:33 +0000

    Updated RSAKeys.java to generate RSA key pairs, and getters for each key values

--- src/main/java/com/tom/ipautoupdatemod/RSAKeys.java
+++ src/main/java/com/tom/ipautoupdatemod/RSAKeys.java
@@ -1,6 +1,25 @@
 package com.tom.ipautoupdatemod;
 
+import java.io.IOException;
+import java.security.*;
+
 public class RSAKeys {
-    private void generateKeys(){}
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+    private void generateKeys() throws NoSuchAlgorithmException, IOException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        KeyPair pair = keyGen.generateKeyPair();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+    }
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
 
 }
