22d2fb8f1e008fea78617bd801801f6b6d248fca
Tom Wu <tomwu370@gmail.com>
2023-12-01 19:57:50 +0000

    Updated Database.java to include useful snippets for HttpClient usage

--- src/main/java/com/tom/ipautoupdatemod/Database.java
+++ src/main/java/com/tom/ipautoupdatemod/Database.java
@@ -1,30 +1,61 @@
 package com.tom.ipautoupdatemod;
-    import java.sql.*;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONArray;
+
 
 public class Database {
     // write to database by connecting to external link
     final String connectionString;
 
     public Database(String connectionString) {
-        this.connectionString = connectionString;
+        this.connectionString = connectionString; // the url for http requests
     }
 
-    public static void main(String args[])
-            throws SQLException, ClassNotFoundException
-    {
-        Class.forName("com.mysql.jdbc.Driver");
-        try (Connection con = DriverManager
-                .getConnection("jdbc:mysql://localhost:3306/myDb", "user1", "pass")) {
-            // use con here
+
+    public static void main(String args[]) throws URISyntaxException, IOException, InterruptedException {
+        String urlParameters = "asd";
+        String url = "https://jsonplaceholder.typicode.com/posts";
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
+                .header("accept", "application/json")
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(
+                request,
+                HttpResponse.BodyHandlers.ofString()
+        );
+        String body = response.body();
+
+        // simple json conversion
+        Gson g = new Gson();
+        String s = g.toJson(body);
+        System.out.println(s);
+
+        // array of json conversion
+        JSONArray array = new JSONArray(body);
+        for (Object jso: array) {
+            System.out.println(jso);
+
         }
+
+
     }
 
     public void Update(String newURL){}
 
-    public String getConnectionString(){return "test";}
-
     public String getURL(){
-        // decrypt from key, if exist then return else don't
+        // decrypt from key, if exist then return, else don't
         return "test";}
 
     public boolean isWhiteList(String username){
--- src/main/java/com/tom/ipautoupdatemod/Host.java
+++ src/main/java/com/tom/ipautoupdatemod/Host.java
@@ -1,9 +1,10 @@
 package com.tom.ipautoupdatemod;
 
 public class Host {
+    private String currentIP;
     public String getNewURL(){return "url";}
 
     public void uploadURL(String uuid){}
 
-    public void updateServerList(){}
+
 }
