133e9cf3eee45dcd16ed690731c7a44b09350493
Tom Wu <tomwu370@gmail.com>
2023-12-01 18:42:00 +0000

    Added encrypt and decrypt methods to Encryptor.java

--- src/main/java/com/tom/ipautoupdatemod/Encryptor.java
+++ src/main/java/com/tom/ipautoupdatemod/Encryptor.java
@@ -1,7 +1,26 @@
 package com.tom.ipautoupdatemod;
 
-public class Encryptor {
-    public String encrypt(){return "encrpted";}
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
-    public String decrypt(){return "decrypted";}
+public class Encryptor {
+    public String encrypt(String data, PublicKey publicKey) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return new String(cipher.doFinal(data.getBytes()));
+    }
+
+    public String decrypt(String data, PrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return new String(cipher.doFinal(data.getBytes()));
+    }
 }
